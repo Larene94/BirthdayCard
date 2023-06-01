b3daf34d1e162b863ddbcc93f64df50b6b18cff8
Jing <larenejing@JINGs-MacBook-Air.local>
2023-06-01 19:12:08 +0100

    Birthday Card

---
+++ main.py
@@ -0,0 +1,13 @@
+Recipient= "Tobe"
+Year = int(2023)
+YOB = int(2000)
+Age = (Year - YOB)
+Message = "May you have a great blessed year ahead"
+Sender= "Larene"
+
+print(f"{Recipient}, let's celebrate your {Age} years of awesomeness!\nWishing you a day filled with joy and laughter as you turn {Age} !\n\n")
+print(f"{Message}\n\n")
+print(f"With love and best wishes,\n{Sender}")
+
+
+
